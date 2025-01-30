45a983813672febcaeca6fbf59a2412f6399d527
HenokTeklu <anomaitech@gmail.com>
2025-01-29 22:15:03 -0700

    kml file is added

--- assets/project3.js
+++ assets/project3.js
@@ -1,5 +1,5 @@
 // Initialize the map
-let map3 = L.map('map3').setView([13.479694, 39.467275], 13);
+let map3 = L.map('map3').setView([13.479694, 39.467275], 19);
 
 // Base layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,14 +7,28 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap'
 }).addTo(map3);
 
-// Load KML using Leaflet Omnivore
+// Load KML file
 omnivore.kml('assets/mekelle.kml')
     .on('ready', function() {
         map3.fitBounds(this.getBounds());
     })
+    .on('layeradd', function(e) {
+        let layer = e.layer;
+        if (layer.feature && layer.feature.properties && layer.feature.properties.name) {
+            layer.bindPopup(`<h3>${layer.feature.properties.name}</h3><p>${layer.feature.properties.description || 'No description available'}</p>`);
+
+            layer.on('click', function() {
+                if (layer.getBounds) {
+                    map3.fitBounds(layer.getBounds()); // Zoom to polygons or lines
+                } else if (layer.getLatLng) {
+                    map3.setView(layer.getLatLng(), 16); // Zoom closer for points
+                }
+            });
+        }
+    })
     .addTo(map3);
 
-// Function to refresh map size when tab is clicked
+// Function to refresh map size when the tab is clicked
 function refreshMap3() {
     setTimeout(() => {
         map3.invalidateSize();
